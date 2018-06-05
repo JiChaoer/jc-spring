f161380afa0ebc7b6f3754bca0725e9001eb83d2
jichao1 <jichao1@jd.com>
2018-06-05 11:48:40 +0800

    编译版本：1.8

--- src/com/jichao/spring/framework/core/DispatcherServlet.java
+++ src/com/jichao/spring/framework/core/DispatcherServlet.java
@@ -1,12 +1,10 @@
 package com.jichao.spring.framework.core;
 
-import javax.servlet.http.HttpServlet;
-
 /**
  * Title：核心控制器
  *
  * @author jichao
  * @date 2018-06-05
  */
-public class DispatcherServlet extends HttpServlet {
+public class DispatcherServlet {
 }
