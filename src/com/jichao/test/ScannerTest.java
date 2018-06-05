3627226cb53388a6d5747207e828acd805e889f6
jichao1 <jichao1@jd.com>
2018-06-05 10:32:22 +0800

    jdk1.8->lambda

--- src/com/jichao/test/ScannerTest.java
+++ src/com/jichao/test/ScannerTest.java
@@ -2,20 +2,15 @@ package com.jichao.test;
 
 import com.jichao.spring.framework.scanner.PackageScanner;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 public class ScannerTest {
 
     public static void main(String args[]) {
         Map<String, Class<?>> classes = PackageScanner.scanner("com.jichao");
-        Set<Map.Entry<String, Class<?>>> set = classes.entrySet();
-        Iterator<Map.Entry<String, Class<?>>> iterator = set.iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, Class<?>> entry = iterator.next();
-            System.out.println("key: " + entry.getKey());
-            System.out.println("value: " + entry.getValue());
-        }
+        classes.forEach((k, v) -> {
+            System.out.println("key: " + k);
+            System.out.println("value: " + v);
+        });
     }
 }
